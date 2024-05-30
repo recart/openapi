f13abde4a992ec745144a3aff5a70a51f4bc96f4
Máté Huszárik <matehuszarik@gmail.com>
2024-05-30 15:32:41 +0200

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0NTQwMA/spectral.js?branch_slug=mate-huszarik-modified-2-files&token=478bdfb8-df51-48b6-aeb1-581c1cc5834c";
+export default { extends: ruleset };
